7361aa43739fb108cf10c8aae32e45556149f749
Amdad92 <emdadctg92@gmail.com>
2024-12-18 19:44:04 +0600

    add date input field and make it interactive using js

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const taskList = ["Wash Dishes", "Hand washes"];
+const taskList = [{
+    name: "Wash Dishes",
+    dueDate:"2024-12-18"
+},{
+    name:"Hand washes",
+    dueDate:"2024-12-18"
+}];
 let taskBtn = document.getElementById('js-task-btn');
 taskBtn.addEventListener("click", function () {
     addTodo();
 });
 
+renderTodoList();
 function renderTodoList() {
     let todoListHTML = '';
     for (let i = 0; i < taskList.length; i++) {
-        const todo = taskList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = taskList[i];
+        //const name=todoObject.name;
+        //const dueDate=todoObject.dueDate;
+        const{name,dueDate}=todoObject;
+
+        const html = `
+            <p>
+                ${name} ${dueDate} 
+                <button onclick="taskList.splice(${i},1);
+                renderTodoList();
+                ">Delete</button>
+            </p>
+        `;
         todoListHTML += html;
     }
     console.log(todoListHTML);
@@ -18,7 +36,14 @@ function renderTodoList() {
 function addTodo() {
     const inputElement = document.getElementById('js-taskInput');
     const name = inputElement.value;
-    taskList.push(name);
+    const dateInputElement=document.querySelector(".js-due-date-input");
+    const dueDate=dateInputElement.value;
+    taskList.push({
+        //name: name,
+        //dueDate: dueDate,
+        name,
+        dueDate
+    });
     console.log(taskList);
     inputElement.value = '';
     renderTodoList();
